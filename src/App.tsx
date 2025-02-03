5e766345f7b2ecb3d31d4d14cd0a5342840317e2
Banjoluwafemi <femmybanjoh@gmail.com>
2025-02-03 12:14:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const track = async () => {
 function App() {
   const steps = [
     {
-      content: "Heyyyyy, angel.",
+      content: "Heyyyyy, Angel.",
       image: "/character/one.png",
     },
     {
