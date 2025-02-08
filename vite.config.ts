b53e632d30f80fd6e17325a3c2ce9ef159c9e7ff
Banjoluwafemi <femmybanjoh@gmail.com>
2025-02-08 21:57:14 +0000

    Fixed Vite output directory for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // ✅ Ensure Vite outputs to "dist"
   server: {
     port: 6263,
     open: true,
