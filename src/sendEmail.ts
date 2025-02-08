9e961f04315b7979ec005a8396610a88d3612749
Banjoluwafemi <femmybanjoh@gmail.com>
2025-02-08 01:12:30 +0100

    Update sendEmail.ts

--- src/sendEmail.ts
+++ src/sendEmail.ts
@@ -7,13 +7,13 @@ const sendEmail = async (recipients: string[]) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.femmybanjoh@gmail.com,
-      pass: process.env.Femostic1@
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
   });
 
   const mailOptions = {
-    from: process.env.femmybanjoh@gmail.com,
+    from: process.env.EMAIL_USER,
     to: recipients,
     subject: 'She Said Yes!',
     text: 'Will you be my girlfriend? She said yes!',
