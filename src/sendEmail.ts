d796f5b66568650b5d94ca65bca7d473c5d3e038
Banjoluwafemi <femmybanjoh@gmail.com>
2025-02-08 02:07:50 +0100

    Update sendEmail.ts

--- src/sendEmail.ts
+++ src/sendEmail.ts
@@ -1,29 +1,25 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
 const sendEmail = async (recipients: string[]) => {
-  // Create a transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // You can use any email service here
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER, // Your email address from environment variable
-      pass: process.env.EMAIL_PASS // Your email password from environment variable
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
   });
 
-  // Setup email data with unicode symbols
   const mailOptions = {
-    from: process.env.EMAIL_USER, // Sender address
-    to: recipients, // List of recipients
-    subject: 'She Said Yes!', // Subject line
-    text: 'Will you be my girlfriend? She said yes!', // Plain text body
-    html: '<strong>Will you be my girlfriend? She said yes!</strong>' // HTML body
+    from: process.env.EMAIL_USER,
+    to: recipients,
+    subject: 'She Said Yes!',
+    text: 'Will you be my girlfriend? She said yes!',
+    html: '<strong>Will you be my girlfriend? She said yes!</strong>'
   };
 
-  // Send mail with defined transport object
   try {
     await transporter.sendMail(mailOptions);
     console.log('Emails sent successfully');
